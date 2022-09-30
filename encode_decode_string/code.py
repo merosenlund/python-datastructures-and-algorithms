33cf7d2bfbcd573502c63e0f0826b35b17d68b55
Mitch <merosenlund@gmail.com>
2022-09-29 21:32:08 -0700

    Redid encode decode and added longest consecutive squence

--- encode_decode_string/code.py
+++ encode_decode_string/code.py
@@ -1,25 +1,38 @@
+# Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
+
+# Please implement encode and decode
+
+# Input: ["lint","code","love","you"]
+# Output: ["lint","code","love","you"]
+# Explanation:
+#                                 01234567
+# One possible encode method is: "4#lint4#code4#love4#you"
+#                                 i s
+#                                  j
+
 """
 @param: strs: a list of strings
 @return: encodes a list of strings to a single string.
 """
 def encode(strs):
     res = ""
-    for s in strs:
-        res += str(len(s)) + "#" + s
+    for string in strs:
+        res += str(len(string)) + "#" + string
+    print(res)
     return res
 
 """
 @param: str: A string
 @return: dcodes a single string to a list of strings
 """
-def decode(str):
+def decode(string):
     res, i = [], 0
-    while i < len(str):
+    while i < len(string):
         j = i
-        while str[j] != "#":
+        while string[j] != "#":
             j += 1
-        count = int(str[i:j])
-        res.append(str[j + 1 : j + count + 1])
-        i = j + count + 1
-
+        length = int(string[i:j])
+        res.append(string[j + 1 : j + length + 1])
+        i = j + length + 1
     return res
+
---
+++ longest_consecutive_sequence/test.py
@@ -0,0 +1,13 @@
+from numbers import Number
+import unittest
+
+import code
+
+class Test(unittest.TestCase):
+    def test_return_type(self):
+        result = code.longestConsecutive([1,4,21,14,22])
+        unittest.assertEquals(Number,)
+
+
+if __name__ == '__main__':
+    unittest.main()
